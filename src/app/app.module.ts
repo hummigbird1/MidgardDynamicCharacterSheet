5a6daa217fa63112612d374e0e433b787f71a75a
hummigbird1 <hummigbird1@gmail.com>
2022-02-27 14:34:03 +0100

    First Session

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NumericValueHandlerComponent } from './components/numeric-value-handler/numeric-value-handler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumericValueHandlerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/numeric-value-handler/numeric-value-handler.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-numeric-value-handler',
+  templateUrl: './numeric-value-handler.component.html',
+  styleUrls: ['./numeric-value-handler.component.css']
+})
+export class NumericValueHandlerComponent implements OnInit {
+
+  @Input() label: string = 'Default';
+  @Input() currentValue: Number = 0;
+  @Input() minValue: Number = 0;
+  @Input() maxValue: Number = 0;
+  @Input() baseValue: Number | string= 0;
+  @Input() showReset: boolean = true;
+  @Output() currentValueChange = new EventEmitter<Number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.resetValue();
+  }
+
+  // TODO MaxWert muss überschreibbar werden wenn man z.B. im Level Up Modus ist
+  countUp() {
+    if(this.currentValue < this.maxValue) {
+      this.currentValue = Number(this.currentValue)+1;
+    }
+  }
+
+  countDown() {
+    if(this.currentValue > this.minValue) {
+      this.currentValue = Number(this.currentValue)-1;
+    }
+  }
+
+  resetValue() {
+    this.currentValue = Number(this.baseValue);
+  }
+}
