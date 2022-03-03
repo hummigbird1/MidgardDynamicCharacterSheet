1026fb818ba59b2c47d5ad7c42a0ebcdd6f0d6df
Engin Yildrim <EYI@indeca.de>
2022-03-03 21:49:42 +0100

    added tabs for sheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NumericValueHandlerComponent } from './components/numeric-value-handler/numeric-value-handler.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgMaterialModule } from './ng-material/ng-material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NumericValueHandlerComponent } from './components/numeric-value-handler
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
