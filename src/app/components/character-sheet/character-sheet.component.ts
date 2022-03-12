e6ff577cb6201ed1009fb8dcc645cab0b5077fb8
hummigbird1 <hummigbird1@gmail.com>
2022-03-12 23:29:37 +0100

    Add character sheet component

---
+++ src/app/components/character-sheet/character-sheet.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Player } from 'src/app/models/player';
+
+@Component({
+  selector: 'app-character-sheet',
+  templateUrl: './character-sheet.component.html',
+  styleUrls: ['./character-sheet.component.css']
+})
+export class CharacterSheetComponent implements OnInit {
+
+  @Input() player!: Player;
+  @Output() playerChange = new EventEmitter<Player>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
